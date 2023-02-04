eceb95577ace344542ef4944c75f94617a7002ce
Daphne <121971548+daphnewan@users.noreply.github.com>
2023-02-03 21:35:38 -0800

    lesson 2.1 update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-Public float horizontalInput;
+public float horizontalInput;
+public float speed=10.0f;
+public float xRange=10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,17 @@ Public float horizontalInput;
     // Update is called once per frame
     void Update()
     {
+//Keep the player in bounds
+if (transform.position.x<-xRange)
+{
+transform.position=new Vector3(-xRange, transform.position.y, transform.position.z);
+}
+
+if (transform.position.x>xRange)
+{
+transform.position=new Vector3(xRange, transform.position.y, transform.position.z);
+}
         horizontalInput=Input.GetAxis("Horizontal");
+transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
     }
 }
