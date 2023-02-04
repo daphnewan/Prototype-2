e0a71575ee415b758ac58816b93f3dc049cac4e8
Daphne <121971548+daphnewan@users.noreply.github.com>
2023-02-04 00:07:58 -0800

    unity updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public float horizontalInput;
 public float speed=10.0f;
 public float xRange=10.0f;
 
+public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,6 @@ public float xRange=10.0f;
     // Update is called once per frame
     void Update()
     {
-//Keep the player in bounds
 if (transform.position.x<-xRange)
 {
 transform.position=new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -29,5 +29,9 @@ transform.position=new Vector3(xRange, transform.position.y, transform.position.
 }
         horizontalInput=Input.GetAxis("Horizontal");
 transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
-    }
+
+if(Input.GetKeyDown(KeyCode.Space))
+{
+Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
 }
+}}
